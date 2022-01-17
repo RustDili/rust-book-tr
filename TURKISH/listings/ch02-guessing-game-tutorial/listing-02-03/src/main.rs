709094116135fa6fce62457101aaf7dfbfeb0e95
RustDili <57715293+RustDili@users.noreply.github.com>
2022-01-17 18:09:56 +0300

    main.rs güncellendi

--- TURKISH/listings/ch02-guessing-game-tutorial/listing-02-03/src/main.rs
+++ TURKISH/listings/ch02-guessing-game-tutorial/listing-02-03/src/main.rs
@@ -8,20 +8,20 @@ fn main() {
     println!("Tuttuğum sayıyı tahmin edin!");
 
     // ANCHOR: ch07-04
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    let gizli_sayı = rand::thread_rng().gen_range(1..101);
     // ANCHOR_END: ch07-04
 
-    println!("Gizli sayı: {}", secret_number);
+    println!("Gizli sayı: {}", gizli_sayı);
 
     println!("Tahmininizi girin.");
 
-    let mut guess = String::new();
+    let mut tahmin = String::new();
 
     io::stdin()
-        .read_line(&mut guess)
+        .read_line(&mut tahmin)
         .expect("Veri okuma hatası!");
 
-    println!("Tahmininiz:: {}", guess);
+    println!("Tahmininiz:: {}", tahmin);
     // ANCHOR: ch07-04
 }
 // ANCHOR_END: ch07-04
