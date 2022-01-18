dcb499b55b1590dcc78832bd8a5ff4cd675979ef
RustDili <57715293+RustDili@users.noreply.github.com>
2022-01-18 03:43:52 +0300

    Create main.rs

---
+++ TURKISH/listings/ch02-guessing-game-tutorial/listing-02-05/src/main.rs
@@ -0,0 +1,45 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Tuttuğum sayıyı tahmin edin!");
+
+    let gizli_sayı = rand::thread_rng().gen_range(1..101);
+
+    println!("Gizli sayı: {}", gizli_sayı);
+
+    loop {
+        println!("Tahmininizi girin.");
+
+        let mut tahmin = String::new();
+
+        // ANCHOR: here
+        // --snip--
+
+        io::stdin()
+            .read_line(&mut tahmin)
+            .expect("Satır okuma hatası!");
+
+        // ANCHOR: ch19
+        let tahmin: u32 = match tahmin.trim().parse() {
+            Ok(sayı) => sayı,
+            Err(_) => continue,
+        };
+        // ANCHOR_END: ch19
+
+        println!("Tahmininiz: {}", tahmin);
+
+        // --snip--
+        // ANCHOR_END: here
+
+        match tahmin.cmp(&gizli_sayı) {
+            Ordering::Less => println!("Sayınız küçük!"),
+            Ordering::Greater => println!("Sayınız büyük!!"),
+            Ordering::Equal => {
+                println!("Kazandınız!");
+                break;
+            }
+        }
+    }
+}
