c7e021b7175f79ce6dcd14b888ce57a099a6cac8
RustDili <57715293+RustDili@users.noreply.github.com>
2022-01-26 01:53:17 +0300

    main.rs yüklendi

---
+++ TURKISH/listings/ch03-common-programming-concepts/no-listing-27-if-false/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // ANCHOR: here
+    let sayı = 7;
+    // ANCHOR_END: here
+
+    if sayı < 5 {
+        println!("Koşul doğru.");
+    } else {
+        println!("Koşul yanlış!");
+    }
+}
