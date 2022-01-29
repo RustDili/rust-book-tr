2b3555d1d6be72c35adeb66603ab0c7f4c5db0f9
RustDili <57715293+RustDili@users.noreply.github.com>
2022-01-29 03:15:30 +0300

    main.rs yüklendi

---
+++ TURKISH/listings/ch03-common-programming-concepts/no-listing-33-return-value-from-loop/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let mut sayaç = 0;
+
+    let sonuç = loop {
+        sayaç += 1;
+
+        if sayaç == 10 {
+            break sayaç * 2;
+        }
+    };
+
+    println!("Sonuç: {}", sonuç);
+}
